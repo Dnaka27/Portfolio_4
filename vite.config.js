3c6bbb55e393218987a6c7dbc413c916a75cc618
Diogo Oike <diogooikekanefukujapan@gmail.com>
2024-08-16 00:24:35 -0300

    Configs conflicts repaired

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Portfolio_4/', // Adicione essa linha
-  plugins: [react()],
-});
->>>>>>> c514c9a6650de453244897fcb33aa5f34b8dcdd9
